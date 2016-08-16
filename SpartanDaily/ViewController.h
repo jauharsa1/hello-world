6c3432462a0fb3e186b1618727039d06106fb764
Sameer Jauhar <jauharsa@sameers-macbook-air.local>
2016-08-16 12:57:20 -0700

    Initial Commit

---
+++ SpartanDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpartanDaily
+//
+//  Created by Sameer Jauhar on 8/16/16.
+//  Copyright © 2016 Sameer Jauhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
