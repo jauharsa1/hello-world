c66feab721197b275ea751f8bacb2e6f080a7827
jauharsa1 <jauharsa1@gmail.com>
2016-08-16 16:19:26 -0700

    first commit

---
+++ Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  SpartanDaily
+//
+//  Created by Sameer Jauhar on 8/16/16.
+//  Copyright © 2016 Sameer Jauhar. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
